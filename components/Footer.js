6781cbe26bafdf50e3e9fda492b8adff5f930225
Abdullah000001 <geniusswork00@gmail.com>
2021-09-22 16:37:32 +0500

    day one build

---
+++ components/Footer.js
@@ -0,0 +1,41 @@
+const Footer = () => {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">About</h5>
+                <p>How Airbnd Works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnd plus</p>
+                <p>Airbnd luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">Community</h5>
+                <p>How Airbnd Works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnd plus</p>
+                <p>Airbnd luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">Host</h5>
+                <p>How Airbnd Works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnd plus</p>
+                <p>Airbnd luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">Support</h5>
+                <p>How Airbnd Works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnd plus</p>
+                <p>Airbnd luxe</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Footer
