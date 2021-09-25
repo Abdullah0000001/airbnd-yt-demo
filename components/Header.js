379966e19b5a809ea9412c3a9f02fed651dc12c9
Abdullah000001 <geniusswork00@gmail.com>
2021-09-26 03:42:42 +0500

    day two build

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,44 @@
 import Image from 'next/image'
-import { SearchIcon , GlobeAltIcon , MenuIcon , UserCircleIcon , UserIcon } from '@heroicons/react/solid'
-const Header = () => {
+import { SearchIcon , GlobeAltIcon , MenuIcon , UserCircleIcon , UserIcon, UsersIcon } from '@heroicons/react/solid'
+import { useState } from 'react'
+import 'react-date-range/dist/styles.css'; 
+import 'react-date-range/dist/theme/default.css';
+import { DateRangePicker } from 'react-date-range';
+import { useRouter } from "next/dist/client/router";
+
+const Header = ({ placeholder }) => {
+    const [searchInput , setSearchInput ] = useState('');
+    const [startDate , setStartDate ] = useState(new Date());
+    const [endDate , setEndDate ] = useState(new Date());
+    const [noOfGuest,setNoOfGuest] = useState(1);
+    const router = useRouter();
+    const selectionRange ={
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection',
+      
+    }
+    const resetInput = () =>{
+        setSearchInput('')
+    }
+    const handleSelect = (ranges) =>{
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
+    }
+    const search = () =>{
+        router.push({
+            pathname: '/Search',
+            query:{
+                location: searchInput,
+                startDate: startDate.toISOString(),
+                endDate: endDate.toDateString(),
+                noOfGuest,
+            }
+        })
+    }
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
-            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+            <div onClick={()=>router.push('/')} className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image 
                     src="https://links.papareact.com/qd3"
                     layout="fill"
@@ -14,7 +49,7 @@ const Header = () => {
             </div>
 
             <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start Your Search"/>
+                <input value={searchInput} onChange={(e)=> setSearchInput(e.target.value)} className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder={placeholder ||"Start Your Search"}/>
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
             </div>
 
@@ -26,7 +61,25 @@ const Header = () => {
                     <UserCircleIcon className="h-6"/>
                 </div>
             </div>
-
+            {searchInput && (
+                <div className="flex flex-col col-span-3 mx-auto">
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        minDate={new Date()}
+                        rangeColors={["#FD5861"]}
+                        onChange={handleSelect}
+                    />
+                    <div className="flex items-center border-b mb-4">
+                        <h2 className="text-2xl flex-grow font-semibold">Number of Guests</h2>
+                        <UsersIcon className="h-5"/>
+                        <input type="number" value={noOfGuest} onChange={(e)=>setNoOfGuest(e.target.value)} min={1} className="w-12 pl-2 text-lg outline-none text-red-400"/>
+                    </div>
+                    <div className="flex">
+                        <button onClick={resetInput} className="flex-grow text-gray-500">Canncel</button>
+                        <button onClick={search} className="flex-grow text-red-400">Search</button>
+                    </div>
+                </div>
+            )}
         </header>
     )
 }
